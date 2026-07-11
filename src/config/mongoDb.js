507809c07b6abd07573402573330f3c5d1eb875d
Kashish Narang <knarang0909@gmail.com>
2026-07-11 17:36:50 +0530

    Fix MongoDB Atlas connection and seller routing

--- src/config/mongoDb.js
+++ src/config/mongoDb.js
@@ -1,15 +1,15 @@
 import { MongoClient } from "mongodb";
 
 let client;
-export  const connectDb=async ()=>{
+export const connectDb = async () => {
     try {
-      client=await  MongoClient.connect(process.env.DB_URL)
-            console.log("MongoDB connected")
-            createIndex(client.db());
+        console.log("DB_URL:", process.env.DB_URL); // <-- Add this
+        client = await MongoClient.connect(process.env.DB_URL);
+        console.log("MongoDB connected");
+        createIndex(client.db());
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-   
 }
  export const getDb=()=>{
         return client.db();
