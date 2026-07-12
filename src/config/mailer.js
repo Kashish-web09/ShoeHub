13ba419749c8c53ac85138c204a3bb9a75c9cb64
Kashish Narang <knarang0909@gmail.com>
2026-07-12 15:49:08 +0530

    fix nodemailer smtp for railway

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -2,11 +2,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 import nodemailer from "nodemailer";
 const transport = nodemailer.createTransport({
-    
-    service: "gmail",
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
+    },
+    tls:{
+        rejectUnauthorized:false
     }
 });
+transport.verify((err,success)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("Mailer ready")
+    }
+})
 export default transport;
